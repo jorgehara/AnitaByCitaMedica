71eea20190b0e98657eee16537bc13a2e2088b78
Jorge Hara Devs <88298206+jorgehara@users.noreply.github.com>
2026-02-01 17:36:39 -0300

    update quite comentario de NO ATIENDE

--- src/app.ts
+++ src/app.ts
@@ -1076,7 +1076,8 @@ const welcomeFlow = addKeyword<Provider, IDBDatabase>(welcomeKeywords)
             console.log('3. Fecha de cita:', formattedDate);
 
             // Mensaje de bienvenida
-            await flowDynamic(`🤖🩺 *¡Bienvenido al Asistente Virtual del Dr.Kulinka!* 🩺 \n\n ‼️*EL DIA MIERCOLES 28 DE ENERO NO ATIENDE POR LA TARDE ‼️*\n\n`);
+            await flowDynamic(`🤖🩺 *¡Bienvenido al Asistente Virtual del Dr.Kulinka!* 🩺 \n\n`);
+                // ‼️*EL DIA MIERCOLES 28 DE ENERO NO ATIENDE POR LA TARDE ‼️*\n\n
 
             // Obtener las citas reservadas
             const reservedTimes = await getReservedAppointments(formattedDate);
@@ -1203,7 +1204,8 @@ const welcomeFlow = addKeyword<Provider, IDBDatabase>(welcomeKeywords)
 
                 message += '\n📝 *Para reservar, responde con el número del horario que deseas*\n ℹ️*Para SOBRETURNOS LLAMAR AL 3735604949*';
                 // message += '\n🏥 Si necesitas un sobreturno, escribe *"sobreturnos"*';
-                message += '\n❌ Para cancelar, escribe *"cancelar"* \n\n ‼️*EL DIA MIERCOLES 28 DE ENERO NO ATIENDE POR LA TARDE ‼️*\n\n';
+                message += '\n❌ Para cancelar, escribe *"cancelar"* \n\n'
+                // ‼️*EL DIA MIERCOLES 28 DE ENERO NO ATIENDE POR LA TARDE ‼️*\n\n';
 
                 await flowDynamic(message);
             } else {
@@ -1296,7 +1298,7 @@ const welcomeFlow = addKeyword<Provider, IDBDatabase>(welcomeKeywords)
                                         (slotHour === currentHour && slotMinute > currentMinute));
                             }
                             return slot.status === 'available';
-                        });
+                        }); 
                     
                     if (availableMorning.length > 0) {
                         morningMessage = `*🌅 Horarios de mañana:*\n`;
